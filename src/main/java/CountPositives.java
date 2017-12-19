a9ccfe0f42a818f681ae79ad02a767836927a6d7
Ola <aleksandra.wardulinska@gmail.com>
2017-12-19 21:00:12 +0100

    tasks from codewars

---
+++ src/main/java/CountPositives.java
@@ -0,0 +1,22 @@
+
+public class CountPositives {
+    public static int[] countPositivesSumNegatives(int[] input) {
+
+        if (input == null || input.length == 0) return new int[]{};
+        int countElements = 0;
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input != null) {
+                if (input[i] > 0) {
+                    countElements++;
+                }
+                if (input[i] < 0) {
+                    sum += input[i];
+                }
+
+            }
+        }
+        return new int[]{countElements, sum};
+    }
+}
+
---
+++ src/main/java/SquareDigit.java
@@ -0,0 +1,33 @@
+import java.util.stream.Collectors;
+
+public class SquareDigit {
+    public int squareDigits(int n) {
+        String result = "";
+
+        while (n != 0) {
+            int digit = n % 10 ;
+            result = digit*digit + result ;
+            n /= 10 ;
+        }
+
+        return Integer.parseInt(result) ;
+    }
+
+    public int squareDigits2(int n) {
+        return Integer.parseInt(String.valueOf(n)
+                .chars()
+                .map(i -> Integer.parseInt(String.valueOf((char) i)))
+                .map(i -> i * i)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining("")));
+    }
+
+}
+/*  Description:
+
+Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out.
+
+Note: The function accepts an integer and returns an integer
+*/
