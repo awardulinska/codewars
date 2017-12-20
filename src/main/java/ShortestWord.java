ab126b61f8829f6374fd9788f85220ea057a9e21
Ola <aleksandra.wardulinska@gmail.com>
2017-12-20 20:06:14 +0100

    correct shortest word

--- src/main/java/ShortestWord.java
+++ src/main/java/ShortestWord.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ShortestWord {
-    public static int findShort1 (String s) {
+    public static int findShort (String s) {
 
     List<String> words = Arrays.asList(s.split(" "));
     String shortestWord = words.stream().min(Comparator.comparing(word -> word.length())).get();
