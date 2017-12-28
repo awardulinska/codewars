dec9e6888dc232a7227dd4e90aac67fcb272843d
Ola <aleksandra.wardulinska@gmail.com>
2017-12-28 16:51:41 +0100

    palindrome

---
+++ src/test/java/PalindromeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromeTest {
+    @Test
+    public void WordsArePalindrome(){
+        assertEquals("kok", "kok");
+        assertEquals("kobylamamalybok", "kobylamamalybok");
+
+    }
+
+}
