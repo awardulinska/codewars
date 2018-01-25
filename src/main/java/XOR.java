6fcc256ccae229f8e4ce6bb14479cc4261a521e9
Ola <aleksandra.wardulinska@gmail.com>
2018-01-25 16:35:20 +0100

    exclusive or

---
+++ src/main/java/XOR.java
@@ -0,0 +1,14 @@
+public class XOR {
+
+    public static boolean xor(boolean a, boolean b) {
+
+        if (a != b ){
+           return true;
+        }
+
+        return false;
+    }
+}
+
+// na szybko:
+// return !(a==b);
